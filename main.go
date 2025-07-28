c95099e97d00c8d9ab4a98580c99f9d77a2d8981
hope <413525149@qq.com>
2025-07-28 23:03:28 +0800

    logger

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"hope_blog/pkg/logger"
 	"hope_blog/router"
 )
 
 func main() {
+	//初始化logger
+	logger.InitLogger()
+	defer logger.Close()
+	//初始化路由
 	route := router.Router()
+	if route != nil {
+		logger.Write("初始化路由成功")
+	}
 	route.Run(":9999")
+
 }
